240f67d8698083595eb5fdf354c1c63be0c6d818
AbdulazizNassar <66841531+AbdulazizNassar@users.noreply.github.com>
2023-02-26 01:15:36 +0300

    Update NavMenu.dart

--- lib/shared/NavMenu.dart
+++ lib/shared/NavMenu.dart
@@ -8,117 +8,122 @@ import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 User? user = AuthService().currentUser;
-final CollectionReference _userCollection =
+CollectionReference _userCollection =
     FirebaseFirestore.instance.collection('users');
+String myUsername = '';
 
-class NavMenu extends StatelessWidget {
+class NavMenu extends StatefulWidget {
   const NavMenu({super.key});
 
   @override
-  Widget build(BuildContext context) => Drawer(
-        child: SingleChildScrollView(
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                buildHeader(context),
-                buildMenuItems(context),
-              ]),
-        ),
-      );
-
-  Widget buildHeader(BuildContext context) => Material(
-        color: Colors.blue,
-        child: InkWell(
-          onTap: () {
-            //close Nav menu
-            Navigator.pop(context);
-
-            // Navigator.of(context).push(MaterialPageRoute(
-            //   builder: (context) =>
-            //   ));
-          },
-          child: Container(
-            color: Colors.blue,
-            padding: EdgeInsets.only(
-              top: 24 + MediaQuery.of(context).padding.top,
-              bottom: 24,
-            ),
-            child: FutureBuilder(
-                future:
-                    _userCollection.where("uid", isEqualTo: user!.uid).get(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return Column(children: [
-                      const FlutterLogo(
-                        size: 80,
-                      ),
-                      const SizedBox(height: 12),
-                      Text(
-                        // ignore: unnecessary_string_interpolations
-                        "${snapshot.data!.docs[0].get("name")!}",
-                        style:
-                            const TextStyle(fontSize: 28, color: Colors.white),
-                      ),
-                      Text(
-                        "${user!.email}",
-                        style:
-                            const TextStyle(fontSize: 16, color: Colors.white),
-                      ),
-                    ]);
-                  }
-                  return Text("loading");
-                }),
-          ),
-        ),
-      );
-  Widget buildMenuItems(BuildContext context) => Container(
-        padding: const EdgeInsets.all(0),
-        child: Wrap(
-          runSpacing: 16, //vertical spacing
-          children: <Widget>[
-            ListTile(
-              leading: const Icon(Icons.home_outlined),
-              title: const Text("Home"),
-              onTap: () => Navigator.of(context).pushReplacement(
-                  MaterialPageRoute(builder: (context) => const Home())),
-            ),
-            ListTile(
-              leading: const Icon(Icons.history_outlined),
-              title: const Text("History"),
-              onTap: () {
-                // Navigator.pop(context);
-                // Navigator.of(context).pushNamed(MaterialPageRoute(
-                //   builder: (context) => const //Page
-                // ));
-              },
-            ),
-            ListTile(
-              leading: const Icon(Icons.message_outlined),
-              title: const Text("Messages"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(Icons.support_agent_outlined),
-              title: const Text("Support"),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: const Icon(Icons.star_border_outlined),
-              title: const Text("Rate Us"),
-              onTap: () {},
-            ),
-            const Divider(color: Colors.black54),
-            ListTile(
-              leading: const Icon(Icons.logout),
-              title: const Text("Log out"),
-              onTap: () {
-                signOutConfirm(context);
-              },
-            ),
-          ],
-        ),
-      );
+  State<NavMenu> createState() => _NavMenuState();
 }
+
+class _NavMenuState extends State<NavMenu> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: SingleChildScrollView(
+        child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildHeader(context),
+              buildMenuItems(context),
+            ]),
+      ),
+    );
+  }
+}
+
+Widget buildHeader(BuildContext context) => Material(
+      color: Colors.blue,
+      child: InkWell(
+        onTap: () {
+          //close Nav menu
+          Navigator.pop(context);
+
+          // Navigator.of(context).push(MaterialPageRoute(
+          //   builder: (context) =>
+          //   ));
+        },
+        child: Container(
+          color: Colors.blue,
+          padding: EdgeInsets.only(
+            top: 24 + MediaQuery.of(context).padding.top,
+            bottom: 24,
+          ),
+          child: FutureBuilder(
+              future: _userCollection.where("uid", isEqualTo: user!.uid).get(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Column(children: [
+                    const FlutterLogo(
+                      size: 80,
+                    ),
+                    const SizedBox(height: 12),
+                    Text(
+                      // ignore: unnecessary_string_interpolations
+                      "${snapshot.data!.docs[0].get("name")!}",
+                      style: const TextStyle(fontSize: 28, color: Colors.white),
+                    ),
+                    Text(
+                      "${user!.email}",
+                      style: const TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ]);
+                }
+                return Text("loading");
+              }),
+        ),
+      ),
+    );
+Widget buildMenuItems(BuildContext context) => Container(
+      padding: const EdgeInsets.all(0),
+      child: Wrap(
+        runSpacing: 16, //vertical spacing
+        children: <Widget>[
+          ListTile(
+            leading: const Icon(Icons.home_outlined),
+            title: const Text("Home"),
+            onTap: () => Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const Home())),
+          ),
+          ListTile(
+            leading: const Icon(Icons.history_outlined),
+            title: const Text("History"),
+            onTap: () {
+              // Navigator.pop(context);
+              // Navigator.of(context).pushNamed(MaterialPageRoute(
+              //   builder: (context) => const //Page
+              // ));
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.message_outlined),
+            title: const Text("Messages"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.support_agent_outlined),
+            title: const Text("Support"),
+            onTap: () {},
+          ),
+          ListTile(
+            leading: const Icon(Icons.star_border_outlined),
+            title: const Text("Rate Us"),
+            onTap: () {},
+          ),
+          const Divider(color: Colors.black54),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text("Log out"),
+            onTap: () {
+              signOutConfirm(context);
+            },
+          ),
+        ],
+      ),
+    );
 
 signOutConfirm(context) {
   Alert(
