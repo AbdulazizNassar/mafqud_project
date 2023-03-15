6ee23f4d746e04641962419da7f4a8e1e2d19b47
AbdulazizNassar <66841531+AbdulazizNassar@users.noreply.github.com>
2023-03-15 16:53:50 +0300

    messaging

---
+++ lib/services/notifications.dart
@@ -0,0 +1,7 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:http/http.dart';
+var token = FirebaseMessaging.instance;
+
+SendNotification(String title, String body, String id) async {
+  await http
+}
