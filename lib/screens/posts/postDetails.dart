70f413d1fdfbaa404bd73fd6c68a2851462cdb87
AbdulazizNassar <66841531+AbdulazizNassar@users.noreply.github.com>
2023-03-19 16:53:57 +0300

    Update postDetails.dart

--- lib/screens/posts/postDetails.dart
+++ lib/screens/posts/postDetails.dart
@@ -26,6 +26,7 @@ class _postDetailsState extends State<postDetails> {
             Map<String, dynamic> data =
                 snapshot.data!.data() as Map<String, dynamic>;
             return Scaffold(
+              backgroundColor: Colors.grey.shade200,
               appBar: AppBar(
                 title: Text("${widget.posts['title']}"),
                 backgroundColor: Colors.blue[900],
@@ -38,61 +39,109 @@ class _postDetailsState extends State<postDetails> {
                 const SizedBox(
                   height: 300,
                 ),
-                Row(children: const [
-                  Icon(
-                    Icons.pin_drop_outlined,
-                    size: 40,
-                  ),
-                  //Todo edit to make location written by user
-                  Text("Ksu", style: textStyle),
-                ]),
-                const SizedBox(height: 15),
                 Container(
-                    margin: const EdgeInsets.only(right: 250),
-                    color: Colors.grey,
-                    child: Text(
-                      "status: ${widget.posts["status"]}",
-                      style: textStyle,
-                    )),
-                const SizedBox(height: 15),
-                Row(
-                  children: [
-                    const Icon(
-                      Icons.timer_outlined,
-                      size: 30,
+                  margin: const EdgeInsets.only(top: 20),
+                  width: double.infinity,
+                  height: 350,
+                  decoration: const BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(40),
+                      topRight: Radius.circular(40),
                     ),
-                    Text(
-                      readTimestamp(widget.posts["Date"]),
-                      style: textStyle,
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 1),
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                  margin: const EdgeInsets.fromLTRB(0, 30, 190, 0),
-                  color: Colors.blue,
-                  child: Text(
-                    "Description: \n ${widget.posts["description"]}",
-                    style: textStyle,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
+                        child: Row(
+                          children: [
+                            const Icon(
+                              Icons.pin_drop_outlined,
+                              size: 40,
+                            ),
+                            //Todo edit to make location written by user
+                            const Text("Ksu", style: textStyle),
+                            const SizedBox(
+                              width: 100,
+                            ),
+                            const Icon(
+                              Icons.timer_outlined,
+                              size: 30,
+                            ),
+                            Text(
+                              readTimestamp(widget.posts["Date"]),
+                              style: textStyle,
+                            ),
+                          ],
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        child: Align(
+                          alignment: Alignment.topRight,
+                          child: Row(
+                            children: [
+                              Text(
+                                "Status: ${widget.posts["status"]}",
+                                style: textStyle,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 10, horizontal: 20),
+                        child: Container(
+                          padding: const EdgeInsets.fromLTRB(15, 15, 0, 0),
+                          width: double.infinity,
+                          decoration: const BoxDecoration(
+                              color: Colors.blueGrey,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(40))),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "Description:",
+                                style: textStyle,
+                              ),
+                              const SizedBox(
+                                height: 4,
+                              ),
+                              Text(
+                                '${widget.posts["description"]}',
+                                style: textStyle,
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 40,
+                      ),
+                      Center(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text(
+                              "ad posted by : ${data["name"]}",
+                              style: const TextStyle(fontSize: 20),
+                            ),
+                            ElevatedButton.icon(
+                                onPressed: () {},
+                                icon: const Icon(Icons.message),
+                                label: const Text("Send a Message"))
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 25),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "ad posted by : ${data["name"]}",
-                      style: TextStyle(fontSize: 20),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 20),
-                ElevatedButton.icon(
-                    onPressed: () {},
-                    icon: const Icon(Icons.message),
-                    label: const Text("Send a Message"))
               ]),
             );
           }
