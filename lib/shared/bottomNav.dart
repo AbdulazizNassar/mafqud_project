a0b571a3910c1a9b4548c5c9d2f0f24532aebe81
AbdulrhmanAlangari <123514002+AbdulrhmanAlangari@users.noreply.github.com>
2023-06-06 17:35:06 +0300

    update bottomnav

--- lib/shared/bottomNav.dart
+++ lib/shared/bottomNav.dart
@@ -3,6 +3,7 @@ import 'package:mafqud_project/main.dart';
 import 'package:mafqud_project/screens/chat/chat_list.dart';
 import 'package:mafqud_project/screens/posts/history.dart';
 import 'package:mafqud_project/screens/posts/posts.dart';
+import 'package:mafqud_project/shared/constants.dart';
 
 import '../screens/chat/cubit/chat_cubit.dart';
 
@@ -11,43 +12,71 @@ Widget Bottombar(BuildContext context) {
     color: Colors.blue[900],
     padding: const EdgeInsets.all(2),
     child: Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        IconButton(
-          icon: const Icon(
-            Icons.message_outlined,
-            size: 32,
-            color: Color.fromARGB(255, 228, 228, 228),
-          ),
-          onPressed: () {
-            ChatCubit.get(context).getChatList();
-
-            navKey.currentState!.pushReplacement(MaterialPageRoute(
-                builder: (context) => const ChatListScreen()));
-          },
+        InkWell(
+          child: Container(
+          color: Colors.blue[900],
+          width: 130,
+          height: 60,
+          child: ElevatedButton(
+            style: btnStyle,
+            onPressed: (){
+              ChatCubit.get(context).getChatList();
+                            navKey.currentState!
+                  .push(MaterialPageRoute(builder: (context) => const ChatListScreen()));
+            },
+            child: Icon(
+              Icons.message_outlined,
+              size: 32,
+              color: Color.fromARGB(255, 228, 228, 228),
+              
+            ),
+          )
+          
         ),
-        IconButton(
-          icon: const Icon(
-            Icons.home_outlined,
-            size: 32,
-            color: Color.fromARGB(255, 228, 228, 228),
-          ),
-          onPressed: () {
-            navKey.currentState!.pushReplacement(
-                MaterialPageRoute(builder: (context) => const Posts()));
-          },
+        ),InkWell(
+          child: Container(
+          color: Colors.blue[900],
+          width: 130,
+          height: 60,
+          child: ElevatedButton(
+            style: btnStyle,
+            onPressed: (){
+                            navKey.currentState!
+                  .push(MaterialPageRoute(builder: (context) => const Posts()));
+            },
+            child: Icon(
+              Icons.home_outlined,
+              size: 32,
+              color: Color.fromARGB(255, 228, 228, 228),
+              
+            ),
+          )
+          
         ),
-        IconButton(
-          icon: const Icon(
-            Icons.history,
-            size: 32,
-            color: Color.fromARGB(255, 228, 228, 228),
-          ),
-          onPressed: () {
-            navKey.currentState!
-                .push(MaterialPageRoute(builder: (context) => const History()));
-          },
         ),
+        InkWell(
+          child: Container(
+          color: Colors.blue[900],
+          width: 130,
+          height: 60,
+          child: ElevatedButton(
+            style: btnStyle,
+            onPressed: (){
+                            navKey.currentState!
+                  .push(MaterialPageRoute(builder: (context) => const History()));
+            },
+            child: Icon(
+              Icons.history_outlined,
+              size: 32,
+              color: Color.fromARGB(255, 228, 228, 228),
+              
+            ),
+          )
+          
+        ),
+        ), 
       ],
     ),
   );
